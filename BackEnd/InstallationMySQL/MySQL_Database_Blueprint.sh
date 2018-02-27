732572d918c948ece272abb7e3af1032431d74a9
yizongk <yizongk@gmail.com>
2018-02-27 00:59:06 -0500

    Added auto setup of MySQL database, table and its elements

---
+++ BackEnd/InstallationMySQL/MySQL_Database_Blueprint.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+script_dir="/home/yizongk/CunyCODES/Installing_Configure_MySQL"
+mysql_user="root"
+mysql_pass="3382400"
+
+echo ""
+echo "======================================================================"
+echo "Setup Database Blueprint bash script initiated..."
+echo "======================================================================"
+echo ""
+
+echo "----------------------------------------------------------------------"
+echo "COMMAND: 'COMMAND: 'mysql -N -u $mysql_user -p *secret!* < $script_dir/MySQL_Database_Blueprint.batch'"
+echo "----------------------------------------------------------------------"
+mysql -N -u $mysql_user -p$mysql_pass < $script_dir/MySQL_Database_Blueprint.batch
+
+echo "----------------------------------------------------------------------"
+echo "Bash Script Completed!"
+echo "----------------------------------------------------------------------"
